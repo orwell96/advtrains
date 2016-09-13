173829fe5918cef85999f925f397d14ce36c2cc2
orwell96 <mono96.mml@gmail.com>
2016-09-13 16:11:14 +0200

    add electric signal and rotate signals for better placement

--- signals.lua
+++ signals.lua
@@ -15,6 +15,7 @@ for r,f in pairs({on="off", off="on"}) do
 		tiles = {"advtrains_retrosignal.png"},
 		description="Lampless Signal ("..r..")",
 		on_rightclick=switchfunc,
+		sunlight_propagates=true,
 		groups = {
 			choppy=3,
 			not_blocking_trains=1
@@ -26,6 +27,34 @@ for r,f in pairs({on="off", off="on"}) do
 		}},
 		on_rightclick=function(pos, node, clicker)
 			minetest.swap_node(pos, {name = "advtrains:retrosignal_"..f, param2 = node.param2})
+		end,
+	})
+	minetest.register_node("advtrains:signal_"..r, {
+		drawtype = "mesh",
+		paramtype="light",
+		paramtype2="facedir",
+		walkable = false,
+		selection_box = {
+			type = "fixed",
+			fixed = {-1/4, -1/2, -1/4, 1/4, 2, 1/4},
+		},
+		mesh = "advtrains_signal.b3d",
+		tiles = {"advtrains_signal_"..r..".png"},
+		description="Signal ("..r..")",
+		on_rightclick=switchfunc,
+		groups = {
+			choppy=3,
+			not_blocking_trains=1
+		},
+		light_source = 1,
+		sunlight_propagates=true,
+		mesecons = {effector = {
+			["action_"..f] = function (pos, node)
+				minetest.swap_node(pos, {name = "advtrains:signal_"..f, param2 = node.param2})
+			end
+		}},
+		on_rightclick=function(pos, node, clicker)
+			minetest.swap_node(pos, {name = "advtrains:signal_"..f, param2 = node.param2})
 		end,
 	})
 end
