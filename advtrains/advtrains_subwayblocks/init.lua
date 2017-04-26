0468da1a7393280ebd944cff92fd9a5077122b6a
Gabriel Pérez-Cerezo <gabriel@gpcf.eu>
2017-04-26 21:30:53 +0200

    Added Mountain Railway logo

--- advtrains/advtrains_subwayblocks/init.lua
+++ advtrains/advtrains_subwayblocks/init.lua
@@ -1,5 +1,11 @@
+minetest.register_node("advtrains_subwayblocks:mr", {	description = "Mountain Railway",
+								tiles = {"advtrains_subwayblocks_mr_top.png", "advtrains_subwayblocks_mr_top.png","advtrains_subwayblocks_mr.png","advtrains_subwayblocks_mr.png","advtrains_subwayblocks_mr.png","advtrains_subwayblocks_mr.png"},
+								groups = {cracky=3},
+								light_source=10,
+								paramtype=light,
+})
 minetest.register_node("advtrains_subwayblocks:germany", {	description = "German subway sign",
-								tiles = {"advtrains_subwayblocks_u_top.png", "advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png"},
+								tiles = {"advtrains_subwayblocks_u_top.png", "advtrains_subwayblocks_u_top.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png"},
 								groups = {cracky=3},
 								light_source=10,
 								paramtype=light,
