59f26fa044b7fc25ab2b41967c3c6ddb1e642d36
Gabriel Pérez-Cerezo <gabriel@gpcf.eu>
2017-04-26 16:05:18 +0200

    Subway blocks light up

--- advtrains/advtrains_subwayblocks/init.lua
+++ advtrains/advtrains_subwayblocks/init.lua
@@ -1,4 +1,6 @@
 minetest.register_node("advtrains_subwayblocks:germany", {	description = "German subway sign",
 								tiles = {"advtrains_subwayblocks_u_top.png", "advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png","advtrains_subwayblocks_u-bahn-sign.png"},
 								groups = {cracky=3},
+								light_source=10,
+								paramtype=light,
 })
