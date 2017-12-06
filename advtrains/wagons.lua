e9c51dfab40806c32346f461c52587236977408d
orwell96 <mono96.mml@gmail.com>
2017-12-06 21:27:04 +0100

    Make sure an old_velocity is always passed
    Fixes crash on placing a new subway wagon

--- advtrains/wagons.lua
+++ advtrains/wagons.lua
@@ -519,7 +519,7 @@ function wagon:on_step(dtime)
 				self:update_animation(gp.velocity, self.old_velocity)
 			end
 			if self.custom_on_velocity_change then
-				self:custom_on_velocity_change(gp.velocity, self.old_velocity)
+				self:custom_on_velocity_change(gp.velocity, self.old_velocity or 0)
 			end
 		end
 		
