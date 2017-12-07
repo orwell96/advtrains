875eb90acbc07636a02d28f8002e369b631eb961
mbb <mbb-mail@gmx.de>
2017-12-07 21:29:17 +0100

    Improve textures and models of industrial train and add new more powerful industrial engine

--- advtrains_train_industrial/init.lua
+++ advtrains_train_industrial/init.lua
@@ -13,22 +13,23 @@ advtrains.register_wagon("engine_industrial", {
 	max_speed=20,
 	seats = {
 		{
-			name=S("Driver Stand (left)"),
-			attach_offset={x=-5, y=10, z=-10},
-			view_offset={x=0, y=10, z=0},
+			name=S("Driver Stand (right)"),
+			attach_offset={x=5, y=7, z=-8},
+			view_offset={x=5.2, y=-4, z=0},
 			driving_ctrl_access=true,
 			group = "dstand",
 		},
 		{
-			name=S("Driver Stand (right)"),
-			attach_offset={x=5, y=10, z=-10},
-			view_offset={x=0, y=10, z=0},
+			name=S("Driver Stand (left)"),
+			attach_offset={x=5, y=7, z=-8},
+			view_offset={x=-5.2, y=-4, z=0},
 			driving_ctrl_access=true,
 			group = "dstand",
 		},
 	},
 	seat_groups = {
 		dstand={
+            
 			name = "Driver Stand",
 			access_to = {},
 		},
@@ -41,6 +42,43 @@ advtrains.register_wagon("engine_industrial", {
 	drops={"default:steelblock 4"},
 	horn_sound = "advtrains_industrial_horn",
 }, S("Industrial Train Engine"), "advtrains_engine_industrial_inv.png")
+--big--
+advtrains.register_wagon("engine_industrial_big", {
+	mesh="advtrains_engine_industrial_big.b3d",
+	textures = {"advtrains_engine_industrial_big.png"},
+	drives_on={default=true},
+	max_speed=30,
+	seats = {
+		{
+			name=S("Driver Stand (right)"),
+			attach_offset={x=5, y=7, z=20},
+			view_offset={x=5.2, y=-4, z=11},
+			driving_ctrl_access=true,
+			group = "dstand",
+		},
+		{
+			name=S("Driver Stand (left)"),
+			attach_offset={x=5, y=7, z=-8},
+			view_offset={x=-5.2, y=-4, z=0},
+			driving_ctrl_access=true,
+			group = "dstand",
+		},
+	},
+	seat_groups = {
+		dstand={
+            
+			name = "Driver Stand",
+			access_to = {},
+		},
+	},
+	assign_to_seat_group = {"dstand"},
+	visual_size = {x=1, y=1},
+	wagon_span=4,
+	is_locomotive=true,
+	collisionbox = {-1.0,-0.5,-1.0, 1.0,2.5,1.0},
+	drops={"default:steelblock 4"},
+	horn_sound = "advtrains_industrial_horn",
+}, S("Big Industrial Train Engine"), "advtrains_engine_industrial_inv.png")
 advtrains.register_wagon("wagon_tank", {
 	mesh="advtrains_wagon_tank.b3d",
 	textures = {"advtrains_wagon_tank.png"},
